2c5ed1f7c2fe59b188e712398a9039cc79fa5818
Felipe Zepeda <pipe.zep.cof@gmail.com>
2022-04-09 19:47:11 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -205,12 +205,12 @@ Pair * nextMap(HashMap * map) {
             }
             else
             {
-                if(i==map->capacity-1)
+                i=(i+1)%map->capacity;
+                if(i==map->capacity)
                 {
                     map->current=i;
                     return NULL;
                 }
-                i=(i+1)%map->capacity;
             }
         }
 
