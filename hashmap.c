0392dd625600c48431dd41aaf5bb0d1a5e5830d9
Felipe Zepeda <pipe.zep.cof@gmail.com>
2022-04-09 17:48:26 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -156,10 +156,6 @@ Pair * searchMap(HashMap * map,  char * key) {
 Pair * firstMap(HashMap * map) {
     if(map->buckets[0] != NULL)
     {
-        if(map->buckets[0]->key==NULL)
-        {
-            return NULL;
-        }
         map->current=0;
         return map->buckets[0];
     }
@@ -171,10 +167,6 @@ Pair * firstMap(HashMap * map) {
         {
             if(map->buckets[i]!=NULL)
             {
-                if(map->buckets[i]->key==NULL)
-                {
-                    return NULL;
-                }
                 map->current=i;
                 return map->buckets[i];
             }
